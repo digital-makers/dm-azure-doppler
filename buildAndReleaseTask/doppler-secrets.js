cb19eb284ab0f3a342a112d82a93f7a04a57d1ec
Chris Samios <c.samios@digitalmakers.io>
2022-04-03 09:16:55 +1000

    updated to remove dependency on shell installed doppler 4

--- buildAndReleaseTask/doppler-secrets.js
+++ buildAndReleaseTask/doppler-secrets.js
@@ -6,12 +6,21 @@ module.exports.getSecrets = async () => {
 
     
     const serviceToken = tl.getInput('serviceToken');
-    const response = await axios.get(`https://api.doppler.com/v3/configs/config/secrets/download?format=json`, { 
-        auth: {
-            username: serviceToken,
-            password: ''
-        } 
+
+    const response = await axios({
+        "method": "GET",
+        "url": "https://api.doppler.com/v3/configs/config/secrets/download",
+        "params": {
+            "format": "json"
+        },
+        "auth": {
+            "username": serviceToken,
+            "password": "aaa"
+        }
     });
+
+    // echo some non sensitive data so we know it worked.
+    console.log(`${response.data.DOPPLER_PROJECT}, ${response.data.DOPPLER_ENVIRONMENT}, ${response.data.DOPPLER_CONFIG}`)
 
     return response.data
 }
